cec027bbdbbf3cf3f216fe759bea34dae447388c
sadie100 <03sadie@gmail.com>
2024-07-22 00:30:20 +0900

    feat: 룰렛 마커 구현, CSS 커스텀

--- src/components/roulette/Roulette.tsx
+++ src/components/roulette/Roulette.tsx
@@ -1,11 +1,17 @@
+import Marker from "./Marker";
 import styles from "./roulette.module.css";
+import { useState } from "react";
 
 const Roulette = () => {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const startAnimation = () => {
+    setIsAnimating(true);
+    setTimeout(() => setIsAnimating(false), 1000); // Animation duration
+  };
   return (
-    <div className={styles["compsoul-body"]}>
-      <div className={styles["compsoul-roulette"]}>
-        <div className={styles["roulette-marker"]}></div>
-      </div>
+    <div className={styles["compsoul-roulette"]}>
+      <Marker isAnimating={isAnimating} startAnimation={startAnimation} />
     </div>
   );
 };
